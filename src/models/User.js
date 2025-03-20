a3a73cd9ed31066a6b2c0c0ad4092414eb686d48
TunAnhhaui <anhkyohauik17@gmail.com>
2025-03-20 15:03:54 +0700

    initial commit

---
+++ src/models/User.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+// hash password
+userSchema.pre("save", async function (next) {
+  // Nếu mật khẩu không thay đổi, bỏ qua việc mã hóa
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+// Compare password for func
+userSchema.methods.comparePassword = async function (userPassword) {
+  return await bcrypt.compare(userPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,104 @@
+import express from "express";
+import cloudinary from "../lib/cloudinary.js";
+import protectRoute from "../middleware/auth.middleware.js";
+import Book from "../models/Book.js";
+
+const router = express.Router();
+
+// create a new book
+router.post("/", protectRoute, async (req, res) => {
+  try {
+    const { title, caption, rating, image } = req.body;
+
+    if (!image || !title || !caption || !rating) {
+      return res
+        .status(400)
+        .json({ message: "Vui lòng không bỏ trống các ô nhập!" });
+    }
+
+    // upload image to cloudinary
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
+    //save to db
+    const newBook = new Book({
+      title,
+      caption,
+      rating,
+      image: imageUrl,
+      user: req.user._id,
+    });
+    await newBook.save();
+
+    return res.status(201).json(newBook);
+  } catch (error) {
+    console.log("Error creating book", error);
+
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Phân trang? 5 book 1 trang
+// const response = await fetch("http://localhost:3000/api/books?page=1&limit=5")
+
+router.get("/", protectRoute, async (req, res) => {
+  try {
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 5;
+    const skip = (page - 1) * limit;
+
+    //desc: lấy book mới tạo cho lên đầu trang
+    const books = await Book.find()
+      .sort({ createdAt: -1 }) //desc
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "username profileImage"); // Chỉ lấy username và profileImage theo user
+
+    const totalBooks = await Book.countDocuments(); // Lấy tổng số book được lấy
+
+    res.send({
+      books,
+      currentPage: page,
+      totalBooks,
+      totalPages: Math.ceil(totalBooks / limit), //Math.ceil: làm tròn số
+    });
+  } catch (error) {
+    console.log("Error in get all books route", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.delete("/:id", protectRoute, async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+
+    if (!book) {
+      return res.status(404).json({ message: "Không tìm thấy sách" });
+    }
+
+    // Check creator of the book
+    if (book.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Không có quyền truy cập" });
+    }
+
+    // Delete image from cloudinary
+    // Link 1 cloudinary: https://res.cloudinary.com/name/image/upload/v.../nameImage.png
+    // Tách chuỗi bới dấu '/' sau đó dùng pop để lấy phần từ cuois cùng
+    if (book.image && book.image.includes("cloudinary")) {
+      try {
+        const publicId = book.image.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(publicId);
+      } catch (deleteError) {
+        console.log("Error deleting image from cloudinary", deleteError);
+      }
+    }
+
+    await book.deleteOne();
+
+    res.json({ message: "Sách đã được xóa thành công" });
+  } catch (error) {
+    console.log("Error deleting book", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+export default router;
